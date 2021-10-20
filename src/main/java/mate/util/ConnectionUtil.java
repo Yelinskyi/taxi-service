2c7a95cc4ce328e96aca6f7c13c9e63b9b799ab8
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-10-20 23:35:06 +0300

    connect to remote DB

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi or another URL";
-    private static final String USERNAME = "root or another username";
-    private static final String PASSWORD = "Your password";
+    //private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    private static final String URL
+            = "jdbc:mysql://remotemysql.com:3306/SQifJTkyXf";
+    private static final String USERNAME = "SQifJTkyXf";
+    private static final String PASSWORD = "x50QR6nsGD";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
